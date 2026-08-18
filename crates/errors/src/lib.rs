745b6bf94199cd921222495addc1d9355b9c10d2
saians07 <suberlinsinaga@gmail.com>
2026-08-18 09:57:30 +0700

    add custom error capabilities

--- crates/errors/src/lib.rs
+++ crates/errors/src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::Deserialize;
+use std::borrow::Cow;
+use thiserror::Error;
+
+#[derive(Deserialize, Error, Debug)]
+pub enum CustomErr {
+    #[error("Operation failed: {operation}")]
+    #[serde(skip)]
+    Operation {
+        operation: Cow<'static, str>,
+        #[source]
+        source: Option<Box<dyn std::error::Error + Send + Sync>>,
+    },
+
+    #[error("No sufficient privilege to access the resources.")]
+    #[serde(skip)]
+    UNAUTHENTICATED,
+
+    #[error("This resource is not available for now.")]
+    #[serde(skip)]
+    FORBIDDEN,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl CustomErr {
+    pub fn operation<S, E>(operation: S, source: E) -> Self
+    where
+        S: Into<Cow<'static, str>>,
+        E: std::error::Error + Sync + Send + 'static,
+    {
+        CustomErr::Operation {
+            operation: operation.into(),
+            source: Some(Box::new(source)),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn operation_ori<S: Into<Cow<'static, str>>>(operation: S) -> Self {
+        CustomErr::Operation {
+            operation: operation.into(),
+            source: None,
+        }
+    }
+
+    pub fn error_code(&self) -> i32 {
+        match self {
+            CustomErr::FORBIDDEN => 403,
+            CustomErr::UNAUTHENTICATED => 401,
+            CustomErr::Operation {
+                operation: _,
+                source: _,
+            } => 501,
+        }
     }
 }
