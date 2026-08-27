1c1671a0fefd9dec32701638330910c5cf148667
saians07 <suberlinsinaga@gmail.com>
2026-08-28 04:44:55 +0700

    Add error to the worker execute function.

--- crates/schedulers/src/lib.rs
+++ crates/schedulers/src/lib.rs
@@ -1,7 +1,9 @@
+use std::error::Error;
+
 use tokio_cron_scheduler::{Job, JobScheduler};
 
 pub struct Supervisor {
-    pub scheduler: JobScheduler
+    pub scheduler: JobScheduler,
 }
 
 impl Supervisor {
@@ -11,21 +13,26 @@ impl Supervisor {
         }
     }
 
-    pub async fn get_schedule<T: Worker + Send + Sync + Clone + 'static>(&self, worker: &T) -> Box<str> {
+    pub async fn get_schedule<T: Worker + Send + Sync + Clone + 'static>(
+        &self,
+        worker: &T,
+    ) -> Box<str> {
         worker.get_schedule().await
     }
 
     pub async fn activate_worker<T: Worker + Send + Sync + Clone + 'static>(
         &self,
         worker: T,
-    ) {
+    ) -> Result<(), DynError> {
         let schedule = self.get_schedule(&worker).await;
         let job = Job::new_async(schedule, move |_uuid, _l| {
             let worker_c = worker.clone();
-            Box::pin(async move { worker_c.execute().await })
+            Box::pin(async move { worker_c.execute().await.unwrap() })
         })
         .unwrap();
         self.scheduler.add(job).await.unwrap();
+
+        Ok(())
     }
 
     pub async fn start(&self) -> () {
@@ -33,8 +40,10 @@ impl Supervisor {
     }
 }
 
+type DynError = Box<dyn Error + Send + Sync>;
+
 #[async_trait::async_trait]
 pub trait Worker {
-    async fn execute(&self) -> ();
+    async fn execute(&self) -> Result<(), DynError>;
     async fn get_schedule(&self) -> Box<str>;
 }
